6840a00392f2aac96ab6f87c37ed3ca0d9af1be8
unknown <mahmoudabdalghany@outlook.com>
2019-08-27 15:17:16 +0200

    Removed wrongly typed code.

--- proc6502/main.cpp
+++ proc6502/main.cpp
@@ -4,27 +4,5 @@
 
 int main()
 {
-	double x[3][4] = {
-		{1, 2, 3, 5},
-		{2, 4, 6, 10},
-		{5, 2, 7, 1}
-	};
 
-	for (int i = 0; i < 3; ++i)
-	{
-		double piv = x[i][i];
-		for (int j = 0; j < 4; j++)
-		{
-			x[i][j] /= piv;
-		}
-	}
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			std::cout << x[i][j] << ' ';
-		}
-		std::cout << std::endl;
-	}
 }
